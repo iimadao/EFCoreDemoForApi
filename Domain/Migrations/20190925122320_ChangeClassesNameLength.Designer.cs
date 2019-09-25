802d989b2ea9e658f3f8c91ba9a0e6da25f8ce80
Lligc <iimadao@qq.com>
2019-09-25 20:26:20 +0800

    测试     partial class SchoolDbContextModelSnapshot : ModelSnapshot  是否生效

--- Domain/Migrations/20190925122320_ChangeClassesNameLength.Designer.cs
+++ Domain/Migrations/20190925122320_ChangeClassesNameLength.Designer.cs
@@ -3,17 +3,16 @@ using System;
 using Domain;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Domain.Migrations
 {
-    /// <summary>
-    /// 这个 貌似没有用
-    /// </summary>
     [DbContext(typeof(SchoolDbContext))]
-    partial class SchoolDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190925122320_ChangeClassesNameLength")]
+    partial class ChangeClassesNameLength
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Domain/Migrations/20190925122436_ChangeClassesNameLength2.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Domain.Migrations
+{
+    public partial class ChangeClassesNameLength2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Classes",
+                maxLength: 125,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 200,
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Classes",
+                maxLength: 200,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 125,
+                oldNullable: true);
+        }
+    }
+}
